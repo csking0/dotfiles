a9646c31ed16e94eea55c9ca947f4ebf5257ab0b
Cameron King <cameron.s.king@gmail.com>
2021-04-25 15:30:40 -0700

    04-25-2021 update

--- linux/update.sh
+++ linux/update.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-location='~/Documents/dotfiles/'
-date='date +'%m-%d-%Y''
+location='~/Documents/dotfiles'
+date=$(date +'%m-%d-%Y')
 
-cp ~/.bash_aliases "$location"
-cp ~/.bashrc "$location"
-cp ~/.condarc "$location"
-cp ~/.config/neofetch/config.conf "$location"
-cp ~/.kde/share/config/breezerc "$location"
-cp ~/.kde/share/config/kdeglobals "$location"
-cp ~/.profile "$location"
+cp ~/.bash_aliases "$location"/.bash_aliases
+cp ~/.bashrc "$location"/.bashrc
+cp ~/.condarc "$location"/.condarc
+cp ~/.config/neofetch/config.conf "$location"/config.conf
+cp ~/.kde/share/config/breezerc "$location"/breezerc
+cp ~/.kde/share/config/kdeglobals "$location"/kdeglobals
+cp ~/.profile "$location"/.profile
 git add *
 git commit -m "$date update"
 git push origin master
